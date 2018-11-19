d23749a970e06b55d564c974e2470332a959fa64
takutohfl <mogerio2626@gmail.com>
2018-11-19 08:38:04 +0000

    add full basic test

--- scripts/buzzer4.py
+++ scripts/buzzer4.py
@@ -15,25 +15,7 @@ def write_freq(hz=0):
 def recv_buzzer(data):
     write_freq(data.data)
 
-def exec_music(goal): #追加1 passは何もしないの意味→変更
-    r = MusicResult()
-    fb = MusicFeedback()
-
-    for i, f in enumerate(goal.freqs):
-        fb.remaining_steps = len(goal.freqs) - i
-        music.publish_feedback(fb)
-
-        if music.is_preempt_requested():
-            write_freq(0)
-            r.finished = False
-            music.set_preempted(r)
-            return
-
-        write_freq(f)
-        rospy.sleep(1.0 if i >= len(goal.durations) else goal.durations[i])
-
-    r.finished = True
-    music.set_succeeded(r)
+def exec_music(goal): pass #追加1
 
 if __name__ == '__main__':
     rospy.init_node('buzzer')
